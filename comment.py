a8b012fa1ffa68f1b18de607341b2d3bf01ec9a8
Danish Belal <1886637+danishbelal@users.noreply.github.com>
2021-12-01 11:03:24 +0100

    Untracked unnecessary python script

--- comment.py
+++
@@ -1,8 +0,0 @@
-# Lese die LICENSE Datei und präfixiere jede Zeile
-# mit "// ".
-#
-# Damit kann die präfixierte Datei direkt in C++ Quellen
-# kopiert werden.
-with open("license-header", "r") as in_file, open("LICENSE-commented", "w") as out_file:
-    for line in in_file:
-        out_file.write("// " + line)
